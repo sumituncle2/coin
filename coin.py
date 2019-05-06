31443c5e21e643947e37cc1175a23580fdc24bcb
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-06 18:12:41 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -6,7 +6,7 @@ app = Client('835349563:AAEraMgAMwRFzOdv7kMLVOYSRgYBL-mlTwA')
 
 
 @app.on_message(Filters. command('toss'))
-def randheadtain(client,telegram.Bot.getChatAdministrators, message):
+def randheadtain(client, message):
             message.reply(random.choice(['🤷 Coin flipped: Head', '🤷 Coin flipped: Tail']))
 
 
