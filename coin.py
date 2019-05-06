5c9f00f22206bf7e62916798926096d2f751e300
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-06 10:34:10 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -19,7 +19,8 @@ def ran(client, message):
 
 @app.on_message(Filters. command('show'))
 def randheadtain(client, message):
-    client.send_message(message.chat.id, random.choice(['A','1','2','3','4','5','6','7','8','9','10','J','K','Q']))
+    client.send_message(message.chat.id, [random.choice(['A','1','2','3','4','5','6','7','8','9','10','J','K','Q'])for i in range(3)])
+
 
 
 app.run()
