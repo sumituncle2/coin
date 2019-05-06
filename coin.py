d09020380bd31ff2e1045b69699ada3958318b96
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-06 10:18:48 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -4,13 +4,13 @@ import random
 app = Client('835349563:AAEraMgAMwRFzOdv7kMLVOYSRgYBL-mlTwA')
 @app.on_message(Filters. command('coin'))
 def randheadtain(client, message):
-    client.send_message(message.chat.id, random.choice(['head', 'tail']))
+    client.message.reply(message.chat.id, random.choice(['head', 'tail']))
 
 
 @app.on_message(Filters.command('roll'))
 def ran(client, message):
  if len(message.text.split(' ')) > 1:
-  client.send_message(message.chat.id, random.choice(range(1, int(message.text.split(' ')[1]))))
+  client.message.reply(message.chat.id, random.choice(range(1, int(message.text.split(' ')[1]))))
  else:
   message.reply('add a number bro')
 
