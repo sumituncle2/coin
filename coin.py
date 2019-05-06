76f56f193de489f850359e4d3c90eccddf4b1e68
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-06 09:57:33 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -5,7 +5,11 @@ app = Client('835349563:AAEraMgAMwRFzOdv7kMLVOYSRgYBL-mlTwA')
 @app.on_message(Filters. command('coin'))
 def randheadtain(client, message):
     client.send_message(message.chat.id, random.choice(['head', 'tail']))
+
+
 @app.on_message(Filters.command('roll'))
 def ran(client, message):
-    client.send_message(message.chat.id, int(random.choice(range(1, message.text.split(' ')[1]))))
+    client.send_message(message.chat.id, int(random.choice(range(1, int(message.text.split(' ')[1])))))
+
+
 app.run()
