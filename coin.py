14c650ee65c5b670266c7bfd05c54282976dbcf7
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-06 12:12:14 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -20,7 +20,7 @@ def ran(client, message):
 import random
 @app.on_message(Filters. command('show'))
 def show(client, message):
-    client.send_message(message.chat.id, [random.choice(Your Cards: ['A','1','2','3','4','5','6','7','8','9','10','J','K','Q'])for i in range(3)])
+    client.send_message(message.chat.id,Your Cards : [random.choice(['A','1','2','3','4','5','6','7','8','9','10','J','K','Q'])for i in range(3)])
 
 
 app.run()
