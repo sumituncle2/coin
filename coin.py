263d3f4d783e5268db27dedaabc934cd441b1c2f
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-07 11:16:10 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -22,6 +22,12 @@ def randheadtain( client, message) :
 
         
 
+def user_admin(func):
+    @wraps(func)
+    def is_admin(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user  # type: Optional[User]
+        if user and is_user_admin(update.effective_chat, user.id):
+            return func(bot, update, *args, **kwargs)
         
 
 
