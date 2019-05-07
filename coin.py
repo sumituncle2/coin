7dd622d85e1a321154099d358935ab6681274140
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-07 15:22:47 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, Filters
+from pyrogram import Client, Filters, emoji
 import random
 
 
@@ -10,7 +10,28 @@ from typing import Optional, List
 
 
 
+
 app = Client('835349563:AAEraMgAMwRFzOdv7kMLVOYSRgYBL-mlTwA')
+
+
+
+
+
+MENTION = "[{}](tg://user?id={})"
+MESSAGE = "{} Welcome to [Pyrogram](https://docs.pyrogram.ml/)'s group chat {}!"
+
+chats_filter = Filters.chat(["PyrogramChat", "PyrogramLounge"])
+
+
+@Client.on_message(chats_filter & Filters.new_chat_members)
+def welcome(client, message):
+    new_members = [MENTION.format(i.first_name, i.id) for i in message.new_chat_members]
+    text = MESSAGE.format(Emoji.SPARKLES, ", ".join(new_members))
+    message.reply(text, disable_web_page_preview=True)
+
+
+
+
 
 
 
