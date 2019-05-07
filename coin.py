b71c82d1b2bed63132047088bcbe112d0ad0091e
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-07 18:27:41 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -37,8 +37,7 @@ app = Client('835349563:AAEraMgAMwRFzOdv7kMLVOYSRgYBL-mlTwA')
 
 
 @app.on_message(Filters. command('toss'))
-def randheadtain( client, message) :
-    get_chat_member(chat_id,user_id)
+def get_chat_member(chat_id,user_id) :
     message.reply(’your name user_id')
     message.reply(random.choice(['💫 Result : Tail ', '💫 Result : Head ']))
 
