5943eaa21a4f75a69ce8fdca790946dc3bbf0e05
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-07 09:29:47 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -24,7 +24,7 @@ def randheadtain( client, message) :
 
 
 @app.on_message(Filters. command('admin'))
- def adminlist( client, chat_id, user_id)
+ def promote( client, message)
       chat = update.effective_chat  # type: Optional[Chat]
     message = update.effective_message  # type: Optional[Message]
     user = update.effective_user
