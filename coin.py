fa6cbe4189ce86e62aa0cf27f9c7ce8bd94b84bf
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-05-08 09:12:14 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -38,7 +38,8 @@ app = Client('835349563:AAEraMgAMwRFzOdv7kMLVOYSRgYBL-mlTwA')
 
 @app.on_message(Filters. command('toss'))
 def randheadtain(client, message) :
-    message.reply(random.choice(['💫 Result : **Tail**', '💫 Result :**Head** ']))
+  if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
+       message.reply(random.choice(['💫 Result : **Tail**', '💫 Result :**Head** ']))
 
         
 
@@ -51,7 +52,8 @@ def randheadtain(client, message) :
 
 @app.on_message(Filters. command('gun'))
 def randheadtain( client, message) :
-    message.reply(random.choice(['💫 Result : **AK47**😎', '💫 Result : **Muflis**💥 ']))
+    if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
+       message.reply(random.choice(['💫 Result : **AK47**😎', '💫 Result : **Muflis**💥 ']))
 
 
 
@@ -59,6 +61,7 @@ def randheadtain( client, message) :
 
 @app.on_message(Filters. command('side'))
 def randheadtain(client, message):
+    if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
             message.reply(random.choice(['💫 Result :** Up** 👆 ', '💫 Result : **Down** 👇 ','💫 Result :** Left** 👈','💫 Result : **Right** 👉']))
 
 
@@ -68,15 +71,18 @@ def randheadtain(client, message):
 
 @app.on_message(Filters. command('sps'))
 def randheadtain(client, message):
+       if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
             message.reply(random.choice(['💫 Result :** Paper** ', '💫 Result : **Stone** ','💫 Result : **Sessiors**']))
 
             
 @app.on_message(Filters. command('decide'))
 def randheadtain(client, message):
+    if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
             message.reply(random.choice(['💫 Result :** Yes** ', '💫 Result : **Maybe** ','💫 Result :** No** ']))
 
 app.on_message(Filters. command('start'))
 def randheadtain(client, message):
+  if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
     message.reply( 'Get /help to help for buy Contact - @google_console ')
 
 
@@ -87,19 +93,21 @@ def ran(client, message):
    message.reply(random.choice(range(1, int(message.text.split(' ')[1]))))
   else:
    message.reply('Please set a range!')
- else:
-  message.reply('You are not admin')
+
+ 
 
 
 
 
 @app.on_message(Filters. command('dice'))
 def randheadtain(client, message):
+  if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
     message.reply(random.choice(['👨‍🎓 Your Luck : 1⃣','👨‍🎓 Your Luck : 2⃣','👨‍🎓 Your Luck : 3⃣','👨‍🎓 Your Luck : 4⃣','👨‍🎓 Your Luck  : 5⃣','👨‍🎓 Your Luck : 2⃣','👨‍⚕ Your Luck : 6⃣']))
 
 
 @app.on_message(Filters. command('help'))
 def randheadtain(client, message):
+  if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
     message.reply('My commands : /toss , /gun , /side , /roll {range} ,/sps , /dice , /dice2 , /show , /show1 , /show2 , /decide Need Help Contact - @google_console ')
 
 
@@ -114,6 +122,7 @@ def randheadtain(client, message):
 
 @app.on_message(Filters. command('show'))
 def show(client, message):
+if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
     message.reply(random.choice([ '👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 3⃣','👨‍🎓 Your Card : 4⃣','👨‍🎓 Your Card : 5⃣','👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 6⃣','👨‍🎓 Your Card : 7⃣','👨‍🎓 Your Card : 8⃣','👨‍🎓 Your Card : 9⃣','👨‍🎨 Your Card : 🔟','🧛‍♂ Your Card : 🇦​','🤴 Your Card : 🇰','👨‍🎨 Your Card : 🇯​','👸 Your Card : 🇶​']))
     message.reply(random.choice([ '👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 3⃣','👨‍🎓 Your Card : 4⃣','👨‍🎓 Your Card : 5⃣','👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 6⃣','👨‍🎓 Your Card : 7⃣','👨‍🎓 Your Card : 8⃣','👨‍🎓 Your Card : 9⃣','👨‍🎨 Your Card : 🔟','🧛‍♂ Your Card : 🇦​','🤴 Your Card : 🇰','👨‍🎨 Your Card : 🇯​','👸 Your Card : 🇶​'])) 
     message.reply(random.choice([ '👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 3⃣','👨‍🎓 Your Card : 4⃣','👨‍🎓 Your Card : 5⃣','👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 6⃣','👨‍🎓 Your Card : 7⃣','👨‍🎓 Your Card : 8⃣','👨‍🎓 Your Card : 9⃣','👨‍🎨 Your Card : 🔟','🧛‍♂ Your Card : 🇦​','🤴 Your Card : 🇰','👨‍🎨 Your Card : 🇯​','👸 Your Card : 🇶​']))
@@ -121,17 +130,20 @@ def show(client, message):
 
 @app.on_message(Filters. command('dice2'))
 def randheadtain(client, message):
+   if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
     message.reply(random.choice(['👨‍🎓 Your Luck : 1⃣','👨‍🎓 Your Luck : 2⃣','👨‍🎓 Your Luck : 3⃣','👨‍🎓 Your Luck : 4⃣','👨‍🎓 Your Luck  : 5⃣','👨‍🎓 Your Luck : 2⃣','👨‍⚕ Your Luck : 6⃣']))
     message.reply(random.choice(['👨‍🎓 Your Luck : 1⃣','👨‍🎓 Your Luck : 2⃣','👨‍🎓 Your Luck : 3⃣','👨‍🎓 Your Luck : 4⃣','👨‍🎓 Your Luck  : 5⃣','👨‍🎓 Your Luck : 2⃣','👨‍⚕ Your Luck : 6⃣']))
 
 
 @app.on_message(Filters. command('show2'))
 def show(client, message):
+   if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
     message.reply(random.choice([ '👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 3⃣','👨‍🎓 Your Card : 4⃣','👨‍🎓 Your Card : 5⃣','👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 6⃣','👨‍🎓 Your Card : 7⃣','👨‍🎓 Your Card : 8⃣','👨‍🎓 Your Card : 9⃣','👨‍🎨 Your Card : 🔟','🧛‍♂ Your Card : 🇦​','🤴 Your Card : 🇰','👨‍🎨 Your Card : 🇯​','👸 Your Card : 🇶​']))
     message.reply(random.choice([ '👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 3⃣','👨‍🎓 Your Card : 4⃣','👨‍🎓 Your Card : 5⃣','👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 6⃣','👨‍🎓 Your Card : 7⃣','👨‍🎓 Your Card : 8⃣','👨‍🎓 Your Card : 9⃣','👨‍🎨 Your Card : 🔟','🧛‍♂ Your Card : 🇦​','🤴 Your Card : 🇰','👨‍🎨 Your Card : 🇯​','👸 Your Card : 🇶​'])) 
 
 @app.on_message(Filters. command('show1'))
 def show(client, message):
+   if client.get_chat_member(message.chat.id , message.from_user.id).status == 'administrator':
     message.reply(random.choice([ '👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 3⃣','👨‍🎓 Your Card : 4⃣','👨‍🎓 Your Card : 5⃣','👨‍🎓 Your Card : 2⃣','👨‍🎓 Your Card : 6⃣','👨‍🎓 Your Card : 7⃣','👨‍🎓 Your Card : 8⃣','👨‍🎓 Your Card : 9⃣','👨‍🎨 Your Card : 🔟','🧛‍♂ Your Card : 🇦​','🤴 Your Card : 🇰','👨‍🎨 Your Card : 🇯​','👸 Your Card : 🇶​']))
     
 
