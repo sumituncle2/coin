123a96993d7a37aff7cfb1b921758ad247d0c9f0
Surendra Bro <39272531+funsaddabot@users.noreply.github.com>
2019-05-08 10:47:29 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -38,7 +38,7 @@ app = Client('835349563:AAEraMgAMwRFzOdv7kMLVOYSRgYBL-mlTwA')
 
 @app.on_message(Filters. command('toss'))
 def ran(client, message) :
-    if client.get_chat_member(message.chat.id , message.from_user.id).status == ('administrator' , 'creator'):
+    if client.get_chat_member(message.chat.id , message.from_user.id).status == 'creator':
        message.reply(random.choice(['💫 Result : **Tail**', '💫 Result :**Head** ']))
 
         
